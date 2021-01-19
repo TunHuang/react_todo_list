365208153bcdbfe0a8bab435147caaa66d0a40cb
TunHuang <huang.tun.anton@gmail.com>
2021-01-19 10:38:52 +0100

    kleine Änderungen in Navbar und Hilfe

--- src/components/page/Hilfe.js
+++ src/components/page/Hilfe.js
@@ -5,6 +5,7 @@ const Hilfe = () => {
     <ul className="hilfeliste">
       <li>Neue Aufgabe in die ToDo-Liste einfügen: Aufgabe in das Eingabefeld eingeben, dann auf den Button 'Add Task' klicken</li>
       <li>Aufgabe aus der ToDo-Liste entfernen: Rechts auf das <b>✕</b> klicken, um die dazugehörige Aufgabe aus der Liste zu entfernen.</li>
+      <li>Nummer-Trivia holen: Eine natürliche Zahl in das Eingabefeld eingeben, dann auf den Button 'Trivia' klicken</li>
     </ul>
   )
 };
--- src/components/page/Navbar.js
+++ src/components/page/Navbar.js
@@ -1,18 +1,18 @@
 import './Navbar.css';
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <ul className="navbar">
       <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/hilfe">Hilfe</Link>
+        <Link to="/">ToDo</Link>
       </li>
       <li>
         <Link to="/trivia">Nummer Trivia</Link>
+      </li>
+      <li>
+        <Link to="/hilfe">Hilfe</Link>
       </li>
       <li>
         <Link to="/nav4">Nav 4</Link>
