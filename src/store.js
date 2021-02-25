0d913cd3ad5a906efcb196e4b47563fa4feb40ef
TunHuang <huang.tun.anton@gmail.com>
2021-02-25 13:52:37 +0100

    store.js umgeschrieben, damit die app auf heroku ohne devtools läuft

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/rootReducer';
 
 const middleware = [thunk];
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(...middleware));
+// const composedEnhancer = composeWithDevTools(applyMiddleware(...middleware));
+
+const composedEnhancer = applyMiddleware(...middleware);
 
 const store = createStore(rootReducer, composedEnhancer);
 
