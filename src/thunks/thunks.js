ac12133d70ca7eb9c2f73fdfa912ba732ee68cf9
Tun Huang <huang.tun.anton@gmail.com>
2023-03-12 16:13:26 +0100

    Füge .env hinzu, in der die Server-Adresse angegeben werden kann, ansonsten wird standardmäßig localhost benutzt. Lege benutzte node-Version auf 16 fest. Ändere nummerTrivia URL zu proxy-Endpunkt im eigenen Server

--- src/thunks/thunks.js
+++ src/thunks/thunks.js
@@ -5,74 +5,82 @@ import {
   actionNummerTriviaHolen,
   actionNummerTriviaFehler,
   actionFetchTaskList,
-  actionErledigtSetzen
+  actionErledigtSetzen,
 } from '../actions/actions';
 
-const URL = 'https://todo-backend-exercise.herokuapp.com/';
+const URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3000/';
 
 // export const aufgabeHinzufuegen = aufgabe => dispatch => dispatch(actionAufgabeHinzufuegen(aufgabe));
 
-export const aufgabeHinzufuegen = (aufgabe, deadline) => dispatch => {
+export const aufgabeHinzufuegen = (aufgabe, deadline) => (dispatch) => {
   const body = {
     task: aufgabe,
     deadline,
     completed: false,
-    userId: 'testId'
+    userId: 'testId',
   };
   const bodyJson = JSON.stringify(body);
-  window.fetch(URL + 'tasks/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: bodyJson
-  })
-  .then(res => res.json())
-  .then(aufgabeMitId => dispatch(actionAufgabeHinzufuegen(aufgabeMitId)))
-  .catch(err => console.log(err))
+  window
+    .fetch(URL + 'tasks/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: bodyJson,
+    })
+    .then((res) => res.json())
+    .then((aufgabeMitId) => dispatch(actionAufgabeHinzufuegen(aufgabeMitId)))
+    .catch((err) => console.log(err));
 };
 
-export const loescheEintrag = (index, id) => dispatch => {
-  window.fetch(URL + 'tasks/' + id, {
-    method: 'DELETE'
-  })
-  .then(res => res.json())
-  .then(res => {
-    if (res.deletedCount !== 1) {
-      throw new Error('Auf Server wurde keine Aufgabe gelöscht');
-    }
-    dispatch(actionLoescheEintrag(index));
-  })
-  .catch(err => console.log(err))
+export const loescheEintrag = (index, id) => (dispatch) => {
+  window
+    .fetch(URL + 'tasks/' + id, {
+      method: 'DELETE',
+    })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.deletedCount !== 1) {
+        throw new Error('Auf Server wurde keine Aufgabe gelöscht');
+      }
+      dispatch(actionLoescheEintrag(index));
+    })
+    .catch((err) => console.log(err));
 };
 
 // export const aufgabenLaden = aufgaben => dispatch => dispatch(actionAufgabenLaden(aufgaben));
 
-export const nummerTriviaHolen = nummer => dispatch => {
-  const apiUrl = "http://numbersapi.com/" + nummer;
-  window.fetch(apiUrl).then(response => {
-    response.text().then(trivia => {
-      dispatch(actionNummerTriviaHolen(trivia))
+export const nummerTriviaHolen = (nummer) => (dispatch) => {
+  // const apiUrl = 'http://numbersapi.com/' + nummer;
+  window
+    .fetch(URL + 'trivia-api/' + nummer)
+    .then((response) => {
+      response.text().then((trivia) => {
+        dispatch(actionNummerTriviaHolen(trivia));
+      });
     })
-  }).catch(error => dispatch(actionNummerTriviaFehler(error.toString())));
+    .catch((error) => dispatch(actionNummerTriviaFehler(error.toString())));
 };
 
-export const fetchTaskList = () => dispatch => window.fetch(URL + 'tasks/')
-.then(res => res.json())
-.then(taskList => dispatch(actionFetchTaskList(taskList)))
-.catch(err => console.log(err));
+export const fetchTaskList = () => (dispatch) =>
+  window
+    .fetch(URL + 'tasks/')
+    .then((res) => res.json())
+    .then((taskList) => dispatch(actionFetchTaskList(taskList)))
+    .catch((err) => console.log(err));
 
-export const erledigtSetzen = (id, isCompleted, index) => dispatch => {
+export const erledigtSetzen = (id, isCompleted, index) => (dispatch) => {
   const body = { completed: isCompleted };
   const bodyJson = JSON.stringify(body);
-  window.fetch(URL + 'tasks/' + id, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: bodyJson
-  })
-  .then(res => res.json())
-  .then(res => dispatch(actionErledigtSetzen(index)))
-  .catch(err => console.log(err))
+  window
+    .fetch(URL + 'tasks/' + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: bodyJson,
+    })
+    .then((res) => res.json())
+    .then((res) => dispatch(actionErledigtSetzen(index)))
+    .catch((err) => console.log(err));
 };
